4fabece0f1d3b8344ef3ce0522993d2a9ab454ce
Wayne Hsiao <chronicqazxc@gmail.com>
2016-01-28 13:24:53 +0800

    Add view

--- FlowDemo/ViewController.m
+++ FlowDemo/ViewController.m
@@ -18,6 +18,12 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     // Commit3
+    UIView *view = [[UIView alloc] initWithFrame:CGRectZero];
+    
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
 }
 
 - (void)didReceiveMemoryWarning {
